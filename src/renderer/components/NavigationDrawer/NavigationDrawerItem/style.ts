4722a7c680cbcaeea79a1d0a29ae13ae38b16376
Adam <adam@atik.cc>
2021-10-22 17:44:49 -0500

    lots of ui changes

--- src/renderer/components/NavigationDrawer/NavigationDrawerItem/style.ts
+++ src/renderer/components/NavigationDrawer/NavigationDrawerItem/style.ts
@@ -5,46 +5,29 @@ import { ITheme } from '~/interfaces';
 import { transparency } from '~/renderer/constants';
 
 export const StyledNavigationDrawerItem = styled.div`
-  padding: 4px 16px;
+  /* padding: 4px 16px; */
   display: flex;
   height: 40px;
   border-radius: 4px;
   align-items: center;
   position: relative;
   cursor: pointer;
-
-  ${({ theme, selected }: { theme?: ITheme; selected?: boolean }) => css`
+  ${({ theme, global }: { theme?: ITheme; global?: boolean }) => css`
     &:hover {
-      background-color: ${theme['pages.lightForeground']
-        ? 'rgba(255, 255, 255, 0.06)'
-        : 'rgba(0, 0, 0, 0.04)'};
+      background-color: ${theme['backgroundColor']};
     }
-
-    &:before {
-      opacity: ${selected ? 1 : 0};
-      background-color: ${theme['pages.lightForeground'] ? 'white' : 'black'};
-    }
+    justify-content: ${global ? 'center' : 'left'};
   `};
-
-  &:before {
-    content: '';
-    position: absolute;
-    left: 0;
-    border-radius: 2px;
-    width: 3px;
-    height: 18px;
-  }
 `;
 
 export const Icon = styled.div`
   height: 24px;
-  width: 24px;
-  min-width: 24px;
   opacity: ${transparency.icons.inactive};
-  margin-right: 16px;
+  margin-right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   ${centerIcon(20)};
 
-  ${({ theme }: { theme?: ITheme }) => css`
-    filter: ${theme['pages.lightForeground'] ? 'invert(100%)' : 'none'};
-  `};
+  ${({ theme }: { theme?: ITheme }) => css``};
 `;
